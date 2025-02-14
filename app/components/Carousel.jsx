821e2465002e2c9b72a661273ae4b61fa15c131a
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2025-02-14 15:22:34 +0200

    mobile responsive

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -1,67 +1,58 @@
 'use client';
 import React from 'react';
-import Image from 'next/image';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function Example() {
   return (
-    <Carousel
-      autoPlay
-      infiniteLoop
-      showStatus={true}
-      showIndicators={true}
-      showThumbs={true}
-      interval={10000}
-   
-    >
-      <div className='lg:flex hidden flex-row mx-40'>
-     <div className='h-1/3  rounded-xl w-1/3 bg-yellow-500  py-2'>
-     <a href=''>
-<div className='text-start h-40 '>ERP Software</div>
-</a>
-     </div>
-  
-     <div className='h-1/3 mx-2 rounded-xl w-1/3 bg-yellow-500  py-2'>
-     <a href=''>
-<div className='text-start h-40 '>ERP Software</div>
-</a>
-     </div>
-  
-     <div className='h-1/3 mx-2 rounded-xl w-1/3 bg-yellow-500  py-2'>
-     <a href=''>
-<div className='text-start h-40 '>ERP Software</div>
-</a>
-     </div>
+    <div className="w-full flex justify-center">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={true}
+        showThumbs={false}
+        interval={5000}
+        className="w-full max-w-sm md:max-w-xl lg:max-w-4xl"
+      >
+        {/* Slide 1 */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+          <div className="rounded-xl bg-yellow-500 p-4 text-center">
+            <a href="">
+              <div className="h-40 flex items-center justify-center text-lg font-semibold">ERP Software</div>
+            </a>
+          </div>
+          <div className="rounded-xl bg-yellow-500 p-4 text-center">
+            <a href="">
+              <div className="h-40 flex items-center justify-center text-lg font-semibold">ERP Software</div>
+            </a>
+          </div>
+          <div className="rounded-xl bg-yellow-500 p-4 text-center">
+            <a href="">
+              <div className="h-40 flex items-center justify-center text-lg font-semibold">ERP Software</div>
+            </a>
+          </div>
+        </div>
 
-      </div>
-     
-     
-      <div>
-     
-      <div className='flex flex-row mx-40 my-3'>
-     <div className='h-1/3  rounded-xl w-1/3 bg-yellow-500  py-2'>
-     <a href=''>
-<div className='text-start h-40 '>ERP Software</div>
-</a>
-     </div>
-  
-     <div className='h-1/3 mx-2 rounded-xl w-1/3 bg-yellow-500  py-2'>
-     <a href=''>
-<div className='text-start h-40 '>ERP Software</div>
-</a>
-     </div>
-  
-     <div className='h-1/3 mx-2 rounded-xl w-1/3 bg-yellow-500  py-2'>
-     <a href=''>
-<div className='text-start h-40 '>ERP Software</div>
-</a>
-     </div>
-
-      </div>
-
-
-      </div>
-    </Carousel>
+        {/* Slide 2 */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+          <div className="rounded-xl bg-yellow-500 p-4 text-center">
+            <a href="">
+              <div className="h-40 flex items-center justify-center text-lg font-semibold">ERP Software</div>
+            </a>
+          </div>
+          <div className="rounded-xl bg-yellow-500 p-4 text-center">
+            <a href="">
+              <div className="h-40 flex items-center justify-center text-lg font-semibold">ERP Software</div>
+            </a>
+          </div>
+          <div className="rounded-xl bg-yellow-500 p-4 text-center">
+            <a href="">
+              <div className="h-40 flex items-center justify-center text-lg font-semibold">ERP Software</div>
+            </a>
+          </div>
+        </div>
+      </Carousel>
+    </div>
   );
 }
